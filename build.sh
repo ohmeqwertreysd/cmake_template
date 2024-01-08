2dc419737cc18e1ef1de897b619861967945a5ab
ohmeqwertreysd <justdarberpro@gmail.com>
2024-01-09 00:16:35 +0300

    fix empty call and ref .sh

--- build.sh
+++ build.sh
@@ -10,23 +10,17 @@ cyan='\033[0;36m'
 # Clear the color after that
 clear='\033[0m'
 
-verbose_echo () {
-    echo "------------------------"
-    echo -e "\t$1"
-    echo "------------------------"
-}
-
 if [ -z $* ]; then
-    verbose_echo "Build all"
+    echo "Build all"
     cmake -S . -B build -DTARGET="all"
     cmake --build build -j 12
     GTEST_COLOR=1 ctest --test-dir build/test --output-on-failure -j12
 elif [ $1 == "build" ]; then
-    verbose_echo "Build"
+    echo "Build"
     cmake -S . -B build -DTARGET="src"
 elif [ $1 = "rebuild" ]; then
     rm -r build/*
     cmake -S . -B build/
 elif [ $1 = "clean" ]; then
-    rm -r build/*
+    cmake --build build -j 12 --target clean
 fi
